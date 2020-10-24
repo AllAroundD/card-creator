80e53fa82e9f45ed51944a031f04e57238af90fe
ense <eddi.n.esteban@gmail.com>
2020-10-24 11:37:12 -0400

    splash in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Splash from './Components'
 
 function App() {
   return (
+    <Splash />
     <div className="app">
       <h1>This is the app</h1>
     </div>
--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Splash = props => {
     return (<>
-        <h1>Card Creator</h1>
+        <div className="jumbotron" style='margin-top:5%' id='jumboIntro'>
+            <h1 className="display-4">Card Generator</h1>
+            <p className="lead">Create cards with their own face properties. Prepare decks of cards.</p>
+            <hr className="my-4" />
+            <p>Protip to screenshot card on Chrome: Inspect element with id=cardPreview → [Windows] Ctrl + Shift + P / [Mac] Cmd + Shift + P → type "node screenshot" → Confirm and save in your directory of choice</p>
+            <a className="btn btn-primary btn-lg" id='createCardInit' onClick='showCardForm(event)' role="button">Create cards</a>
+        </div>
     </>)
 }
 

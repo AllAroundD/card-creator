86e1e51707c1479de108f29834d1492f48b85443
AllAroundD <AllAroundDWeb@gmail.com>
2020-10-25 12:11:03 -0400

    set eslint as default formatter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import Splash from './Components/Splash'
+import Splash from './Components/Splash';
 import Card from './Components/Card';
 
-
 function App() {
-  return (
-    <div className="app">
-      <Splash />
-      <h1>This is the app</h1>
-      This is the main
-      <Card />
-    </div>
-  )
+    return (
+        <div className="app">
+            <Splash />
+            <h1>This is the app</h1>
+            This is the main
+            <Card />
+        </div>
+    );
 }
 
-export default App
+export default App;

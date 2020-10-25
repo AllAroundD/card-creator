5e3d04d9764ca498b5d0c344f46a6cedac4fbc81
AllAroundD <AllAroundDWeb@gmail.com>
2020-10-25 15:02:02 -0400

    updated card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Card.css'
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+
 
 function Card() {
     return (
@@ -7,8 +10,10 @@ function Card() {
             <img src="https://images.freeimages.com/images/large-previews/dfc/hands-card-1238263.jpg" className="card-img-top" alt="card image" />
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
-                <a href="#" className="btn btn-primary">Edit</a>
-                <a href="#" className="btn btn-primary">Delete</a>
+                <div className="card-buttons">
+                    <a href="#" className="card-btn"><EditIcon /></a>
+                    <a href="#" className="card-btn"><DeleteIcon /></a>
+                </div>
             </div>
         </div>
     )

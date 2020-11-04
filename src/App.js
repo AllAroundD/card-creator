823fc7013cf30c7fc282023d9bf4d9f0f9f52814
ense <eddi.n.esteban@gmail.com>
2020-11-04 12:55:38 -0500

    new SVGs for action buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Splash from './Components/Splash';
 // import Card from './Components/Card';
-import LeftDrawer from './Components/LeftDrawer'
+import Actions from './Components/Actions'
 import CardsList from './Components/CardsList';
 
 function App() {
     return (
         <div className="app">
-            <LeftDrawer />
+            <Actions />
             <Splash />
             <h1>This is the app</h1>
             <CardsList />
--- src/Components/Actions.js
+++ src/Components/Actions.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import Box from '@material-ui/core/Box'
+import {ReactComponent as AddCardIcon} from '../add-card-plain.svg'
+import {ReactComponent as AddDeckIcon} from '../add-deck-plain.svg'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,20 +17,20 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
-const LeftDrawer = () => {
+const Actions = () => {
     const classes = useStyles();
 
     return (
     <Box className={classes.root} display='flex' flexDirection='column' position='fixed' top='0' left='0'>
-        <Fab color="primary" aria-label="add-card">
-            <AddIcon />
+        <Fab color="primary">
+            <AddCardIcon width='40%' height='100%'/>
         </Fab>
-        <Fab color='primary' aria-label='add-deck'>
-            <AddIcon />
+        <Fab color='primary'>
+        <AddDeckIcon width='70%' height='100%'/>
         </Fab>
     </Box>
     );
 }
 
 
-export default LeftDrawer
+export default Actions

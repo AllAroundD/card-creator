cd2cde94e19a79dc260ea710ed302fb39b2a1be0
ense <eddi.n.esteban@gmail.com>
2020-11-25 15:21:10 -0500

    other things

--- routes/api/cards.js
+++ routes/api/cards.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const cardsController = require("../../controllers/cardsController");
 
-// Matches with "/api/books"
+// Matches with "/api/cards"
 router.route("/")
     .get(cardsController.findAll)
     .post(cardsController.create);
 
-// Matches with "/api/books/:id"
+// Matches with "/api/cards/:id"
 router
     .route("/:id")
     .get(cardsController.findById)

bb1f0b5461debd0685f8350249d0575342262d3b
ense <eddi.n.esteban@gmail.com>
2020-11-30 16:10:54 -0500

    implemented call of all cards for list, added splay card graphic

--- client/src/Components/CardsList.js
+++ client/src/Components/CardsList.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import Card from './Card'
+import API from '../utils/API'
 import './CardsList.css'
 
-function CardsList() {
+function CardsList(props) {
+    let [cards, setCards] = React.useState([])
+    React.useEffect(() => {
+        loadCards()
+    }, [])
+    async function loadCards() {
+        try {
+            let result = await API.getCards()
+            setCards(result.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
     return (
         <div className="cardslist">
             <p>Cards</p><br />
-            <Card id="5fb94fb7e05e9971eceab3ad" title="Card 1" src="/assets/img/cardsample1.jpg" />
-            <Card id="5fb94fb7e05e9971eceab3ae" title="Card 2" src="/assets/img/cardsample2.jpg" />
+            {cards.map((card) => (
+                <Card id={card._id} title={card.name} src="/assets/img/cardsample1.jpg" />
+            ))}
+            {/* <Card id="5fb94fb7e05e9971eceab3ad" title="Card 1" src="/assets/img/cardsample1.jpg" />
+            <Card id="5fb94fb7e05e9971eceab3ae" title="Card 2" src="/assets/img/cardsample2.jpg" /> */}
         </div>
     )
 }
--- client/src/Components/Drawer.js
+++ client/src/Components/Drawer.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Drawer from '@material-ui/core/Drawer'
 import Button from '@material-ui/core/Button'
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import { ReactComponent as ViewInventory } from '../card-splay-plain.svg'
 
-// const useStyles = makeStyles(theme => ({
-//     drawer: { height: 100 }
-// }))
+const drawerHeight = '30%'
+
+const useStyles = makeStyles(theme => ({
+    drawer: { height: drawerHeight },
+    drawerPaper: { height: drawerHeight }
+}))
 
 export default function DrawerBottom(props) {
     const [state, setState] = React.useState(false)
@@ -17,11 +21,12 @@ export default function DrawerBottom(props) {
 
         setState({ ...state, [anchor]: open });
     };
-    // const classes = useStyles()
+
+    const classes = useStyles()
     return (<>
         <React.Fragment key={anchor}>
-            <Button onClick={toggleDrawer(anchor, true)}>(Splayed Cards Graphic)</Button>
-            <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+            <Button onClick={toggleDrawer(anchor, true)}><ViewInventory width='50px' height='50px' /></Button>
+            <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)} className={classes.drawer} classes={{ paper: classes.drawerPaper }}>
                 {props.children}
             </Drawer>
         </React.Fragment>

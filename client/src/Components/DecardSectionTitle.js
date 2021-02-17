11042af3964a5b6822348a46eea0189e1f494854
eesteban <eddi.n.esteban@gmail.com>
2021-02-17 16:01:17 -0500

    list horizontal scroll; list section css

---
+++ client/src/Components/DecardSectionTitle.js
@@ -0,0 +1,17 @@
+
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import '../styles/Card.css'
+import {Img} from 'react-image'
+
+function DecardSectionTitle({title, src }) {
+
+    return (
+        <div className='card cardMain'>
+            <div className="decard__image rounded-top"></div>
+            <div className="decard__section__title"><h3>{title}</h3></div>
+        </div>
+    )
+}
+
+export default DecardSectionTitle
